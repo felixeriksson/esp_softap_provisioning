90162d580f63a8935984b0001683f0ac49de9779
Felix Eriksson <felix@eflix.se>
2025-09-01 16:17:22 +0200

    Remove deprecated Flutter<->Android interface component (PluginRegistry.Registrar)

--- android/src/main/java/com/omert/esp_softap_provisioning/EspSoftapProvisioningPlugin.java
+++ android/src/main/java/com/omert/esp_softap_provisioning/EspSoftapProvisioningPlugin.java
@@ -11,7 +11,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
@@ -31,20 +30,6 @@ public class EspSoftapProvisioningPlugin implements FlutterPlugin, MethodCallHan
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
     channel = new MethodChannel(flutterPluginBinding.getFlutterEngine().getDartExecutor(), "esp_softap_provisioning");
     channel.setMethodCallHandler(this);
-  }
-
-  // This static function is optional and equivalent to onAttachedToEngine. It supports the old
-  // pre-Flutter-1.12 Android projects. You are encouraged to continue supporting
-  // plugin registration via this function while apps migrate to use the new Android APIs
-  // post-flutter-1.12 via https://flutter.dev/go/android-project-migration.
-  //
-  // It is encouraged to share logic between onAttachedToEngine and registerWith to keep
-  // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
-  // depending on the user's project. onAttachedToEngine or registerWith must both be defined
-  // in the same class.
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "esp_softap_provisioning");
-    channel.setMethodCallHandler(new EspSoftapProvisioningPlugin());
   }
 
   @Override
